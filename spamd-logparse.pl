37158024cab0f365586cec08d6971236cd2d3894
Giovanni Bechis <giovanni@bigio.snb.it>
2015-04-05 01:10:47 +0200

    remove duplicated ip addresses and, for the moment, prints ip on stdout

--- spamd-logparse.pl
+++ spamd-logparse.pl
@@ -4,14 +4,19 @@
 
 use strict;
 
+use Data::Dumper qw(Dumper);
 use Getopt::Std;
-use File::Temp qw/ :mktemp /;
+use List::MoreUtils qw(uniq);
 use Geo::IP;
 use RRDs;
 
 my %opts;
 my $fh_log;
 my $log_file;
+my @log_line;
+my $ip;
+my @ip_addr;
+my @uip_addr;
 
 getopts('hf:g:', \%opts);
 if ( defined $opts{'h'} ) {
@@ -29,4 +34,16 @@ if ( -f $log_file ) {
 } else {
 	die "Can't open $log_file"
 }
+
+while (<$fh_log>) {
+	chomp;
+	@log_line = split(' ', $_);
+	$ip = $log_line[5];
+	$ip =~ s/://;
+	$_ = $ip;
+	next if /^logfile/;
+	push ( @ip_addr, $ip );
+}
+@uip_addr = uniq @ip_addr;
+print Dumper \@uip_addr;
 close($fh_log);

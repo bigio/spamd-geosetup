ae54e20676ba5cc28074c7f8d64b2f04585659e2
Giovanni Bechis <giovanni@bigio.snb.it>
2015-04-04 19:01:38 +0200

    Import of a spamd log parser It displays unique ip addresses with related countries for statistical purposes

---
+++ parse-smapdlog.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# ex:ts=8 sw=4:
+
+TMPFILE=`mktemp` || exit 1
+TMPSORT=`mktemp` || exit 1
+
+$(cat $1 | awk '{print $6 "\n";}' | sed 's/\://g;s/logfile//g' > $TMPFILE)
+$(cat $TMPFILE | sort | uniq >$TMPSORT)
+for IP in $(<$TMPSORT);
+do
+	GEOIPSTATE=$(geoiplookup $IP | awk '{print $5 " " $6 " " $7;}')
+	OUTPUT="Ip: $IP, State: $GEOIPSTATE"
+	echo $OUTPUT
+done
+rm -f $TMPFILE
+rm -f $TMPSORT

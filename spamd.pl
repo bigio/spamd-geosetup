0039314f56c7a95e9290f6a119d058eff661e36e
Giovanni Bechis <giovanni@bigio.snb.it>
2015-08-23 18:48:51 +0200

    Implement logging fix a big in QUIT command handling

--- spamd.pl
+++ spamd.pl
@@ -7,9 +7,11 @@ package spamd;
 use strict;
 use warnings;
 
+use Data::Dumper;
 use Getopt::Std;
 use base qw(Net::Server);
 use Net::Server::Daemonize qw(daemonize);
+use Unix::Syslog qw(:macros :subs);
 
 my %opts = ();
 my $background = 0;
@@ -34,14 +36,33 @@ sub slowprint {
 # Process smtp requests
 sub process_request {
 	my $self = shift;
+	my $start_time = time;
+	my $elapsed_time;
+	# print Dumper $self;
 	slowprint "220 spamd IP-based SPAM blocker";
 	while (<STDIN>) {
 		s/[\r\n]+$//;
 		slowprint "250 spamd Hello, pleased to meet you" if /HELO.*/i;
 		slowprint "250 2.0.0: Ok" if /(MAIL FROM:.*)|(RCPT TO:.*)/i;
-		last if /(QUIT)(\.)/i;
+		last if /(QUIT)|(\.)/i;
 	}
 	slowprint "221 2.0.0: Bye";
+	$elapsed_time = time - $start_time;
+	do_log($self->{'server'}->{'peeraddr'}, $elapsed_time);
+}
+
+sub do_log {
+	my $ip_addr = shift;
+	my $elapsed_time = shift;
+	my $id = 'spamd.pl'; my $fac = 'mail';
+	$fac =~ /^[A-Za-z0-9_]+\z/
+		or die "Suspicious syslog facility name: $fac";
+	my $syslog_facility_num = eval("LOG_\U$fac");
+	$syslog_facility_num =~ /^\d+\z/
+	or die "Unknown syslog facility name: $fac";
+	openlog($id, LOG_PID | LOG_NDELAY, $syslog_facility_num);
+	syslog LOG_INFO, "Spammer %s stuck for %d seconds", $ip_addr, $elapsed_time;
+	closelog();
 }
 
 # option parsing

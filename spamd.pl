33471defac3174eddd9dc209dff163dd3143cc66
Giovanni Bechis <giovanni@bigio.snb.it>
2015-08-23 18:01:38 +0200

    Add options to be able do daemonize and change listening port

--- spamd.pl
+++ spamd.pl
@@ -1,8 +1,25 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl
+
+# ex:ts=8 sw=4:
+
 package spamd;
 
-use base qw(Net::Server);
+use strict;
+use warnings;
 
+use Getopt::Std;
+use base qw(Net::Server);
+use Net::Server::Daemonize qw(daemonize);
+
+my %opts = ();
+my $background = 0;
+my $pid;
+my $port=2525;
+my $listenip='*';
+my $user=undef;
+my $group=undef;
+
+# Print slowly
 sub slowprint {
 	my $text = shift(@_);
 	my $i = 0;
@@ -14,6 +31,7 @@ sub slowprint {
 	print "\015\012";
 }
 
+# Process smtp requests
 sub process_request {
 	my $self = shift;
 	slowprint "220 spamd IP-based SPAM blocker";
@@ -26,4 +44,30 @@ sub process_request {
 	slowprint "221 2.0.0: Bye";
 }
 
-spamd->run(port => 2525, ipv => '*');
+# option parsing
+getopts('bg:p:u:', \%opts);
+if ( defined $opts{'b'} ) {
+	$background = 1;
+	if (defined $opts{'g'} and defined $opts{'u'} ) {
+		$user=$opts{'u'};
+		$group=$opts{'g'};
+	} else {
+		die("Background mode needs user [-u] and group [-g] parameters");
+	}
+}
+
+# Set a port different from default
+if ( defined $opts{'p'} ) {
+	$port = $opts{'p'};
+}
+
+# Daemonize if requested
+if ($background eq 1) {
+	daemonize(
+	$user,
+	$group,
+	$pid
+	);
+}
+spamd->run(port => $port, ipv => $listenip);
+1;
